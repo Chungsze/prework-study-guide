20b678be7337dd0224dd5f2b2a0fe8cd5734fc54
Chungsze <ukrenthouse@gmail.com>
2022-11-27 19:20:35 +0000

    add js file

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+/*function ans1(){document.getElementById("demo1").innerHTML ="Dynamic way to express myself"} 
+ function ans2(){document.getElementById("demo2").innerHTML ="Create something unique";} */
+
+ var topics ="HTML,CSS,Git,JavaScript";
+   console.log(topics);
+
+for (var x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+
+}
+
+ /*var topic = "HTML";
+
+ if(topic === 'HTML'){
+    console.log("Let's study HTML!");
+ }else if(topic === 'CSS'){
+    console.log("Let's study CSS!");
+ }else if(topic ==='Git'){
+    console.log("Let's study Git!");
+ }else if(topic === 'JavaScript'){
+    console.log("Let's study Java!");
+ }else{
+    console.log("Please try again!");
+ }*/
