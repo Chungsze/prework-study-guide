742f114b38c968c666216cf11ab9bc3b4eb1baea
Chungsze <ukrenthouse@gmail.com>
2022-11-27 20:37:11 +0000

    add code on js file and add content on html

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,32 @@
 /*function ans1(){document.getElementById("demo1").innerHTML ="Dynamic way to express myself"} 
  function ans2(){document.getElementById("demo2").innerHTML ="Create something unique";} */
 
- var topics ="HTML,CSS,Git,JavaScript";
-   console.log(topics);
+ var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (var x = 0; x < topics.length; x++){
-    console.log(topics[x]);
+ var randomTopic = topics[Math.floor(Math.random()*topics.length)];
 
-}
+ function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+   }
 
- /*var topic = "HTML";
 
- if(topic === 'HTML'){
+ function selectTopic() {
+  if (randomTopic === 'HTML') {
     console.log("Let's study HTML!");
- }else if(topic === 'CSS'){
+  } else if (randomTopic === 'CSS') {
     console.log("Let's study CSS!");
- }else if(topic ==='Git'){
+  } else if (randomTopic === 'Git') {
     console.log("Let's study Git!");
- }else if(topic === 'JavaScript'){
-    console.log("Let's study Java!");
- }else{
-    console.log("Please try again!");
- }*/
+  } else if (randomTopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }
+ }
+
+ console.log('Here are the topics we learned through Prework:');
+ listTopics();
+ console.log('Which topic should we study first?');
+ selectTopic();
